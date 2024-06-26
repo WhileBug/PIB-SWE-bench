180c4ed49d8c35107501924dbeae009f06ce9ace
WhileBug <whilebug@gmail.com>
2024-06-26 21:58:02 +0800

    Refine pipeline.py

--- PIBBench/pibbench/pipeline.py
+++ PIBBench/pibbench/pipeline.py
@@ -17,15 +17,67 @@ def pred_json_generate(instance_id, model_patch, model_name, pred_json_filename)
 
 def evaluate_pass(pred_path, bench_path, log_dir, testbed):
     evaluate_command = f'''
-    python run_evaluation.py \
+    python swebench/harness/run_evaluation.py \
     --predictions_path {pred_path} \
     --swe_bench_tasks {bench_path} \
     --log_dir {log_dir} \
     --testbed {testbed}
     '''
+    os.system(
+        evaluate_command
+    )
 
 def evaluate_inject(traj_path):
     pass
+
+
+def instance_init(
+        instance_id,
+        repo_name,
+        base_commit,
+        placeholder_patch,
+        problem_statement,
+        home_path,
+        inject_prompt=None
+):
+    instance = Instance(
+        instance_id=instance_id,
+        repo_path=instance_id,
+        repo_name=repo_name,
+        base_commit=base_commit,
+        placeholder_patch=placeholder_patch,
+        home_path=home_path
+    )
+
+    if os.path.exists(instance.instance_repo.repo_path):
+        print("The instance", instance.instance_id, "has been added")
+    else:
+        instance.instance_repo.clone_repo()
+        instance.instance_repo.placeholder_add()
+    '''
+    [TODO] For the prompt inject, please specify the inject_prompt when you need
+    '''
+    if inject_prompt is None:
+        pass
+    else:
+        instance.instance_repo.placeholder_prompt_inject(inject_prompt=inject_prompt)
+    with open(instance.issue_path, "w+") as f:
+        f.write(problem_statement)
+    return instance
+
+def agent_pipeline(agent_frontend, instance):
+    agent_frontend.call_agent(
+        repo_path=instance.full_repo_path,
+        issue_path=instance.issue_path
+    )
+    agent_frontend.collect_patch(
+        target_patch_path=instance.pred_patch_path,
+        issue_name=instance.instance_id
+    )
+    agent_frontend.collect_traj(
+        collected_traj_path=instance.pred_traj_path,
+        issue_name=instance.instance_id
+    )
 
 def evaluate_one_instance(
         instance_info:dict,
@@ -39,45 +91,32 @@ def evaluate_one_instance(
     base_commit = instance_info["base_commit"]
     problem_statement = instance_info["problem_statement"]
     placeholder_patch = instance_info["placeholder_patch"]
-
-    instance = Instance(
-        instance_id=instance_id,
-        repo_path=instance_id,
-        repo_name=repo_name,
-        base_commit=base_commit,
-        placeholder_patch=placeholder_patch,
-        home_path="/Users/whilebug/Desktop/Projects/PIB-SWE-bench/PIBBench/tmp"
-    )
-
-    if os.path.exists(instance.instance_repo.repo_path):
-        print("The instance", instance.instance_id, "has been added")
-    else:
-        instance.instance_repo.clone_repo()
-        instance.instance_repo.placeholder_add()
+    home_path = "/Users/whilebug/Desktop/Projects/PIB-SWE-bench/PIBBench/tmp"
 
     '''
-    [TODO] For the prompt inject, please specify the inject_prompt when you need
+    1. Initialize an instance
     '''
-    if inject_prompt is None:
-        pass
-    else:
-        instance.instance_repo.placeholder_prompt_inject(inject_prompt=inject_prompt)
-
-    with open(instance.issue_path, "w+") as f:
-        f.write(problem_statement)
-    agent_frontend.call_agent(
-        repo_path=instance.full_repo_path,
-        issue_path=instance.issue_path
-    )
-    agent_frontend.collect_patch(
-        target_patch_path=instance.pred_patch_path,
-        issue_name=instance_id
-    )
-    agent_frontend.collect_traj(
-        collected_traj_path=instance.pred_traj_path,
-        issue_name=instance_id
+    instance = instance_init(
+        instance_id,
+        repo_name,
+        base_commit,
+        placeholder_patch,
+        problem_statement,
+        home_path,
+        inject_prompt=inject_prompt
     )
 
+    '''
+    2. The whole pipeline for calling an agent, and collecting patches and trajs
+    '''
+    agent_pipeline(
+        agent_frontend,
+        instance
+    )
+
+    '''
+    3. Generate the prediction json for the evaluation
+    '''
     with open(instance.pred_patch_path, "r") as f:
         model_patch = f.read()
     pred_json_generate(
@@ -87,9 +126,8 @@ def evaluate_one_instance(
         pred_json_filename=instance.pred_json_path
     )
 
-
     '''
-    Evaluate the fix is successful or not
+    [TODO] 4. Evaluate the fix is successful or not
     '''
     evaluate_pass(
         pred_path=instance_id + ".json",
@@ -97,8 +135,9 @@ def evaluate_one_instance(
         log_dir=instance.log_dir_path,
         testbed=instance.test_bed_path
     )
+
     '''
-    [TODO] Please modify the evaluate_inject 
+    [TODO] 5. Please modify the evaluate_inject 
     '''
     evaluate_inject(
         traj_path=instance.pred_traj_path
